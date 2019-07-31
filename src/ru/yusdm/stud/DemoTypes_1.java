a9efcbc42ef169892b13e00df6849310aac46b4c
DmitryYusupov <usikovich@mail.ru>
2019-07-31 10:05:55 +0300

    init commit

---
+++ src/ru/yusdm/stud/DemoTypes_1.java
@@ -0,0 +1,119 @@
+package ru.yusdm.stud;
+
+public class DemoTypes_1 {
+
+    public static void main(String[] args) {
+        //demoPrimitivesNumeric_1();
+        //demoPrimitivesNumeric_2();
+        //demoBooleanPrimitive_3();
+        //demoBooleanPrimitive_4();
+        // demoBoolean_5();
+
+      /*  System.out.println(getCash("Ivan"));
+
+        Integer cash = getCash("Semen");
+        if (cash != null) {
+            System.out.println("Semen's cash " + cash);
+        } else {
+            System.out.println("Doesn't buy a ticket");
+        }*/
+       // demoBoxUnbox_6();
+
+        demoString_7();
+
+    }
+
+    public static void demoPrimitivesNumeric_1() {
+        int a = 5;
+        int b = 5;
+
+        int c = a + b;
+        int d = c - b;
+
+        long cc = 5;
+        c = a * b;
+        c = a / b;
+
+        System.out.println(30 / 5);
+        System.out.println(30 / 7);
+    }
+
+    public static void demoPrimitivesNumeric_2() {
+        float a = 5;
+        float b = 5;
+
+        float c = a + b;
+        float d = c - b;
+
+        double dd = a;
+
+        c = a * b;
+        c = a / b;
+
+        System.out.println(30f / 5);
+        System.out.println(30f / 7);
+    }
+
+    public static void demoBooleanPrimitive_3() {
+        boolean t = true;
+        boolean f = false;
+
+        System.out.println(t && f);
+        System.out.println(t || f);
+    }
+
+    public static Integer getCash(String name) {
+        if ("Ivan".equals(name)) {
+            return 10;
+        }
+
+        if ("Petr".equals(name)) {
+            return 0;
+        }
+
+        return null;
+    }
+
+    public static void demoBooleanPrimitive_4() {
+        boolean b1 = validate("1234") && validate("123") || validate("55");
+    }
+
+    public static void demoBoolean_5() {
+        boolean b1 = validate("1234") & validate("123");
+        System.out.println(b1);
+    }
+
+    public static boolean validate(String s) {
+        System.out.println("Validate " + s);
+
+        if (s.length() > 3) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void demoBoxUnbox_6() {
+        //boxing
+        Integer a = 8;
+
+        //unboxing
+        int i = a;
+
+
+        Integer k = null;
+        if (k != null) {
+            i = k;
+        }
+
+    }
+
+    public static void demoString_7() {
+        String s = "a" + "bcd";
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.contains("bcd"));
+    }
+
+
+}
---
+++ src/ru/yusdm/stud/LangConstructions_2.java
@@ -0,0 +1,153 @@
+package ru.yusdm.stud;
+
+public class LangConstructions_2 {
+
+    public static void main(String[] args) {
+        //demoIfElse_2();
+        // demoFor_3();
+        // demoWhile_4();
+        //demoDoWhile_5();
+        //demoEndLessBreak_6();
+
+        //demoNoSwitchBreak_11();
+
+        calcEquation(1, 2, 3);
+        calcEquation(1, 2, 3);
+        calcEquation(1, 2, 3);
+        calcEquation(1, 2, 3);
+    }
+
+    private static void demoIf_1() {
+        if (System.currentTimeMillis() % 2 == 0) {
+            System.out.println(" % 2 is true");
+        }
+    }
+
+    private static void demoIfElse_2() {
+        if (System.currentTimeMillis() % 2 == 0) {
+            System.out.println(" % 2 is true");
+        } else {
+            System.out.println(" % 2 is false");
+        }
+    }
+
+    private static void demoFor_3() {
+        int result = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("total " + result);
+            result = result + 10;
+        }
+
+        System.out.println("Result " + result);
+    }
+
+    private static void demoWhile_4() {
+        int result = 0;
+
+        while (result < 100) {
+            System.out.println("total " + result);
+            result = result + 10;
+        }
+
+        System.out.println("Result " + result);
+    }
+
+    private static void demoDoWhile_5() {
+        int result = 100;
+
+        do {
+            System.out.println("total " + result);
+            result = result + 10;
+        } while (result < 100);
+
+        System.out.println("Result " + result);
+    }
+
+    private static void demoEndLessBreak_6() {
+        int result = 0;
+
+        for (; ; ) {
+            result += 10;
+
+            if (result == 100) {
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static void demoEndLessReturn_7() {
+        int result = 0;
+
+        for (; ; ) {
+            result += 10;
+
+            if (result == 100) {
+                System.out.println(result);
+                return;
+            }
+        }
+    }
+
+    private static void demoNoSwitch_8() {
+        int age = 18;
+
+        if (age == 18) {
+            System.out.println("Beer");
+        } else if (age == 19) {
+            System.out.println("Good beer");
+        } else if (age == 20) {
+            System.out.println("Best beer");
+        }
+    }
+
+    private static void demoSwitchNoBreak_10() {
+        int age = 18;
+        switch (age) {
+            case 18: {
+                System.out.println("Beer");
+            }
+            case 19: {
+                System.out.println("Good Beer");
+            }
+            case 20: {
+                System.out.println("Best Beer");
+            }
+        }
+
+    }
+
+    private static void demoNoSwitchBreak_11() {
+        int age = 18;
+
+        switch (age) {
+            case 18: {
+                System.out.println("Beer");
+                break;
+            }
+            case 19: {
+                System.out.println("Good Beer");
+                break;
+            }
+
+            case 20:
+            case 21: {
+                System.out.println("Best Beer");
+                break;
+            }
+
+            default: {
+                System.out.println("aNY BEER YOU WANT");
+            }
+        }
+
+    }
+
+    private static void calcEquation(float a, float b, float c) {
+
+    }
+
+
+}
